d033589143e0147a51665d7baf2b88b1a0dcfd1e
Laureen Cruz <laureencrz@outlook.com>
2019-07-08 16:17:58 -0700

    preparing to deploy

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const helmet = require('helmet')
 const cors = require('cors')
 const MOVIES = require('./moviedex-list.json')
 
-//console.log(process.env.API_TOKEN)
+
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -48,7 +49,18 @@ app.get('/movie', function handleGetMovie(req, res) {
     res.json(result)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`listening on PORT 8000`)
